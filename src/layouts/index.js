3d3f6c4869f21eb10b2af0c1a701d5f3f7cf3ec5
wynnxin <wynnxing@qq.com>
2019-03-13 16:04:55 +0800

    feat: add  auth Router

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,54 +1,68 @@
 import React from 'react';
-import { Switch, Route, Redirect, Link, withRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
+import { connect } from 'react-redux';
 import routerData from '../router-config';
 import '../style/navigate.sass';
 import 'antd/dist/antd.css';
+import QyLogin from '../pages/login';
 
 const { Header, Footer } = Layout;
 
-const UserComponent = withRouter(props => {
-  const { location } = props;
-  return (
-    <div>
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            selectedKeys={[location.pathname]}
-            defaultSelectedKeys={['3']}
-            style={{ lineHeight: '64px' }}
-          >
-            {routerData.map(item =>
+class Navigator extends React.Component {
+  render() {
+    const { location, isLogin } = this.props;
+    const routerDisplay = isLogin
+      ? routerData
+      : [
+          {
+            path: '/login',
+            component: QyLogin,
+            name: '登录'
+          }
+        ];
+    return (
+      <div>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              selectedKeys={[location.pathname]}
+              defaultSelectedKeys={['3']}
+              style={{ lineHeight: '64px' }}
+            >
+              {routerDisplay.map(item =>
+                item.component ? (
+                  <Menu.Item key={item.path}>
+                    <Link to={item.path}>{item.name}</Link>
+                  </Menu.Item>
+                ) : null
+              )}
+            </Menu>
+          </Header>
+          <Switch>
+            {routerDisplay.map((item, index) =>
               item.component ? (
-                <Menu.Item key={item.path}>
-                  <Link to={item.path}>{item.name}</Link>
-                </Menu.Item>
+                <Route
+                  key={index}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
               ) : null
             )}
-          </Menu>
-        </Header>
-        <Switch>
-          {routerData.map((item, index) =>
-            item.component ? (
-              <Route
-                key={index}
-                path={item.path}
-                component={item.component}
-                exact={item.exact}
-              />
-            ) : null
-          )}
-          <Redirect exact to="/login" />
-        </Switch>
-        <Footer style={{ textAlign: 'center' }}>
-          CopyRight @2017-2019 青柚工作室 qingyou.njupt.edu.cn
-        </Footer>
-      </Layout>
-    </div>
-  );
-});
+            <Redirect exact to="/login" />
+          </Switch>
+          <Footer style={{ textAlign: 'center' }}>
+            CopyRight @2017-2019 青柚工作室 qingyou.njupt.edu.cn
+          </Footer>
+        </Layout>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({ ...state.userReducer });
 
-export default UserComponent;
+export default connect(mapStateToProps)(Navigator);
